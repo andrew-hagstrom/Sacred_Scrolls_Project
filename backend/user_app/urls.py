0e951650455107190ff0cde534e4b182c26cca9a
Joshua Minchew <jminchew97@gmail.com>
2023-12-27 16:54:55 -0600

    can now authenticate and get a valid Token returned for a user

--- backend/user_app/urls.py
+++ backend/user_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Signup
+from .views import Signup, Login
 
 urlpatterns = [
-    path("signup/", Signup.as_view(), name="signup")
+    path("signup/", Signup.as_view(), name="signup"),
+    path("login/", Login.as_view(), name="login"),
 ]
--- backend/user_app/views.py
+++ backend/user_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
@@ -9,17 +10,42 @@ from .serializers import UserSerializer
 from rest_framework.status import (
     HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
 )
+
 
 # Create your views here.
 class Signup(APIView):
     def post(self, request):
         data = request.data
-        # # create user with request data
+
         serializer = UserSerializer(data=data)
         if serializer.is_valid():
-            user = serializer.save()
+            # user = serializer.save()
+            user = User.objects.create_user(**data)
             token = Token.objects.create(user=user)
-            return JsonResponse({"message":"user created successfully."},status=HTTP_201_CREATED)
-        else:
-            return JsonResponse({"message":serializer.errors}, status=HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                {"message": "user created successfully."}, status=HTTP_201_CREATED
+            )
+            
+        return JsonResponse(
+            {"message": serializer.errors}, status=HTTP_400_BAD_REQUEST
+        )
+
+
+class Login(APIView):
+    def post(self, request):
+        data = request.data
+        username = data.get("username")
+        password = data.get("password")
+        print(username, password)
+        user = authenticate(username=username, password=password)
+        print(user)
+        
+        if user is None:
+            return JsonResponse({"message":"Invalid user credentials provided"}, status=HTTP_401_UNAUTHORIZED)
+        
+        token = Token.objects.get_or_create(user=user)
+        token_str = token[0].key
+        
+        return JsonResponse({"token":token_str, "user":user.username})
