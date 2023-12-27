4ff5898eefa516f38e1f17e9861fb6c9fe7aba3b
BDubOne <bryan.t.bartell@gmail.com>
2023-12-27 15:55:08 -0600

    trying to fix nav css

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <>
-    <NavBar style={{backgroundColor:"black"}} user={user} setUser={setUser} />
+    <NavBar user={user} setUser={setUser} />
     <Container>
      <h1>
       SacredScrolls
