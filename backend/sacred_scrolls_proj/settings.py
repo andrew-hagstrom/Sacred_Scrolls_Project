8fd37e3faba3f36e7299551d5521274effbd90c2
Joshua Minchew <jminchew97@gmail.com>
2023-12-28 14:17:31 -0600

    added view and updated cors

--- backend/sacred_scrolls_proj/settings.py
+++ backend/sacred_scrolls_proj/settings.py
@@ -29,8 +29,8 @@ AUTH_USER_MODEL = 'user_app.User'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'bg_app', 
     'bible_app',
     'quran_app',
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'sacred_scrolls_proj.urls'
