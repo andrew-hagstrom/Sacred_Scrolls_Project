909c444587c8475fa105db511c540e85cea51194
Joshua Minchew <jminchew97@gmail.com>
2023-12-29 10:20:40 -0600

    can now search chapter and verse in arabic and english

--- backend/quran_app/urls.py
+++ backend/quran_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import EngQurChapter, EngQurChapterVerse
 
 urlpatterns = [
-    path('eng/chapter/<str:QurChapterNumber>/', EngQurChapter.as_view(), name="quran chapter"),
-    path('eng/chapter/<str:QurChapterNumber>/verse/<str:QurVerseNumber>/', EngQurChapterVerse.as_view(), name="quran chapter and verse")
+    path('<str:lang>/chapter/<str:QurChapterNumber>/', EngQurChapter.as_view(), name="quran chapter"),
+    path('<str:lang>/chapter/<str:QurChapterNumber>/verse/<str:QurVerseNumber>/', EngQurChapterVerse.as_view(), name="quran chapter and verse")
+    
 ]
