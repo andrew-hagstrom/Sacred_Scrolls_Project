39400a9199427753eb51cd9261632041747ad0bf
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2023-12-30 22:17:35 -0600

    journals, favorites, journal, posts revised, refactored some of Josh's code - let me know what you think

--- backend/posts_app/urls.py
+++ backend/posts_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import AllPostsView, UserPostsView, APostView
 
 post_patterns = [
-    path("", AllPosts.as_view(), name="all_posts"),
+    path("", AllPostsView.as_view(), name="all_posts"),
     path("<int:user_id>/", UserPostsView.as_view(), name="user_posts"),
     path("<int:user_id>/posts/<int:post_id>/", APostView.as_view(), name="posts"),
 ]
