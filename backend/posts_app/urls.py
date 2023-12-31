96db5c312358a0a2708cecdf768967d2ed14a9f6
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2023-12-31 13:26:30 -0600

    further refinement to journal and passages

--- backend/posts_app/urls.py
+++ backend/posts_app/urls.py
@@ -4,5 +4,5 @@ from .views import AllPostsView, UserPostsView, APostView
 post_patterns = [
     path("", AllPostsView.as_view(), name="all_posts"),
     path("<int:user_id>/", UserPostsView.as_view(), name="user_posts"),
-    path("<int:user_id>/posts/<int:post_id>/", APostView.as_view(), name="posts"),
+    path("<int:user_id>/posts/<int:post_id>/", APostView.as_view(), name="apost"),
 ]
