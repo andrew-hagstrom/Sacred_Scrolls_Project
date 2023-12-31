3790ec626f15843dc59d768589419cc702c190fa
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2023-12-31 14:49:42 -0600

    JSON changed to Response in posts

--- backend/posts_app/views.py
+++ backend/posts_app/views.py
@@ -23,14 +23,14 @@ class AllPostsView(APIView):
         posts = Posts.objects.all()
         serialized_posts = PostsSerializer(posts, many=True)
         print(serialized_posts)
-        return JsonResponse({"data": serialized_posts.data})
+        return Response({"data": serialized_posts.data})
 
 class UserPostsView(APIView):
     def get(self, request, user_id):
         posts = Posts.objects.filter(user_id_id=user_id)
         serialized_posts = PostsSerializer(posts, many=True)
         print(serialized_posts)
-        return JsonResponse({"data": serialized_posts.data})
+        return Response({"data": serialized_posts.data})
 
     def post(self, request, user_id):
         data = request.data
@@ -38,13 +38,13 @@ class UserPostsView(APIView):
         post_serializer = PostsSerializer(data=data)
 
         if not post_serializer.is_valid():
-            return JsonResponse({"error": post_serializer.errors})
+            return Response({"error": post_serializer.errors})
         try:
             post_serializer.save()
-            return JsonResponse({"message": post_serializer.data})
+            return Response({"message": post_serializer.data})
         except Exception as e:
             print(e)
-            return JsonResponse({"error": "Error saving to post to database"})
+            return Response({"error": "Error saving to post to database"})
 
 
 class APostView(APIView):
@@ -54,10 +54,10 @@ class APostView(APIView):
             post = Posts.objects.get(id=post_id, user_id_id=user_id)
         except Exception as e:
             print(e)
-            return JsonResponse({"error": "error occured fetching post from user"})
+            return Response({"error": "error occured fetching post from user"})
 
         serialized_post = PostsSerializer(post)
-        return JsonResponse({"data": serialized_post.data})
+        return Response({"data": serialized_post.data})
 
     def put(self, request, user_id, post_id):
         data = request.data
@@ -66,15 +66,15 @@ class APostView(APIView):
             post = Posts.objects.get(user_id_id=user_id, id=post_id)
         except Exception as e:
             print(e)
-            return JsonResponse({"error": "Error while trying to fetch post."})
+            return Response({"error": "Error while trying to fetch post."})
 
         updated_post = PostsSerializer(post, data=request.data, partial=True)
 
         if not updated_post.is_valid():
-            return JsonResponse({"error": updated_post.errors})
+            return Response({"error": updated_post.errors})
 
         updated_post.save()
-        return JsonResponse({"data": updated_post.data})
+        return Response({"data": updated_post.data})
 
     def delete(self, request, user_id, post_id):
         post = Posts.objects.get(user_id_id=user_id, id=post_id)
@@ -83,11 +83,11 @@ class APostView(APIView):
             post.delete()
         except Exception as e:
             print(e)
-            return JsonResponse(
+            return Response(
                 {"error": "An error occured when deleting a post."},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
-        return JsonResponse(
+        return Response(
             {"message": "post deleted sucessfully"},
             status=status.HTTP_200_OK,
         )
