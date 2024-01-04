fc874fa2a932f12988237f16f695b8331028b9ab
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-04 08:38:32 -0600

    changed Posts model and adjusted posts page

--- backend/posts_app/serializers.py
+++ backend/posts_app/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Posts
 
 class PostsSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username', read_only=True)
     class Meta:
         model = Posts
-        fields = ['text','user_id']
+        fields = ['username', 'book', 'chapter', 'verse', 'text','timestamp']
