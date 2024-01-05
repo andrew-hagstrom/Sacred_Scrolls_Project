f95896ed41c14a427d5e41497120ebdd763c99eb
BDubOne <bryan.t.bartell@gmail.com>
2024-01-05 08:04:03 -0600

    working

--- frontend/src/components/BioCard.jsx
+++ frontend/src/components/BioCard.jsx
@@ -6,26 +6,26 @@ import Col from 'react-bootstrap/Col';
 
 
 function BioCard({ imageSrc, name, description, email }) {
-  return (
-    <Card className="bio-card">
-      <Row noGutters>
-        <Col md={6}>
-          <div className="bio-image" style={{ backgroundImage: `url(${imageSrc})` }} />
-        </Col>
-        <Col md={6}>
-          <Card.Body className='bio-text'>
-            <Card.Title>
-                {name}
-            </Card.Title>
-            <Card.Text>
-                {description}
-            </Card.Text>
-            <Button href={`mailto:${email}`} style={{marginTop: '-4%', marginLeft: '88%'}} variant="dark" size="sm">e-mail me</Button>
-          </Card.Body>
-        </Col>
-      </Row>
-    </Card>
-  );
-}
-
-export default BioCard;
+    return (
+      <Card className="bio-card">
+        <Row>
+          <Col md={6} className="d-flex align-items-center justify-content-center">
+            <img src={imageSrc} alt={name} className="bio-image" />
+          </Col>
+          <Col md={6}>
+            <Card.Body className='bio-text'>
+              <Card.Title>
+                  {name}
+              </Card.Title>
+              <Card.Text>
+                  {description}
+              </Card.Text>
+              <Button href={`mailto:${email}`} variant="dark" size="sm">e-mail me</Button>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    );
+  }
+  
+  export default BioCard;
