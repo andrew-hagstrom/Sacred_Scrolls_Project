7f21c4ed590697eceb5e4d3fc83a1591df89bb99
BDubOne <bryan.t.bartell@gmail.com>
2024-01-05 10:06:09 -0600

    backend.env

--- backend/sacred_scrolls_proj/settings.py
+++ backend/sacred_scrolls_proj/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e6*a1k+y#flr4+%d-9wyyyiw$3ntyn-yw#uj(rs-ovaes=4&&^'
+SECRET_KEY = env.get('SECRET_KEY')
 AUTH_USER_MODEL = 'user_app.User'
 
 # SECURITY WARNING: don't run with debug turned on in production!
