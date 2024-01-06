51eb4359069b882983c54a8b9a5d26701293e329
andrew-hagstrom <andrew.hagstrom1@gmail.com>
2024-01-05 20:40:27 -0600

    changes to textfields in posts

--- backend/posts_app/models.py
+++ backend/posts_app/models.py
@@ -5,7 +5,7 @@ class Posts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     book=models.CharField(max_length=500, default='Quran/Bible/BG', null=False, blank=False)
     chapter=models.CharField(max_length=100, default='Chapter', null=False, blank=False)
-    verse=models.CharField(max_length=100, default='Verse', null=False, blank=False)
-    text = models.CharField(max_length=100, default=None, null=False, blank=False)
+    verse=models.TextField(default='Verse', null=False, blank=False)
+    text = models.TextField(max_length=400, default=None, null=False, blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
   
