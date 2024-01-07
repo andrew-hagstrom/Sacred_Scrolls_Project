b3533b70ff319bdf2e735fd4263ca1a28ece473d
BDubOne <bryan.t.bartell@gmail.com>
2024-01-07 08:46:56 -0600

    redo

--- frontend/src/pages/FavoritesPage.jsx
+++ frontend/src/pages/FavoritesPage.jsx
@@ -55,51 +55,53 @@ function FavoritesPage() {
 
   return (
     <>
-  <div className={`fade-in-out ${isVisible ? "" : "fade-out"}`}>
-    <div className="container-fluid">
-      <Row>
-        {/* Favorites List */}
-        <Col md={4} xs={12} className="favorites-list" style={{overflowY: 'auto'}}>
-          <h1 style={{fontSize: '34px'}}>Favorites</h1>
-          {favorites.map((favorite) => (
-            <Row style={{cursor: 'pointer'}} key={favorite.id} onMouseEnter={() => {setToRender(favorite); setPostId(favorite.id)}} onClick={renderHandler}>
-              <div className='list-text'>{favorite.reference}</div>
-            </Row>
-          ))}
-        </Col>
-
-        {/* Card Display */}
-        <Col md={8} xs={12}>
-          {favorites.length === 0 ? 
-            <div className='favorites-page-headers'>
-              <h1>No favorites saved</h1>
-              <h2 style={{textAlign: 'center'}}>Use the <Link to="/text-compare/">Textual Comparison Page</Link> to search for passages</h2>
+    <div className={`fade-in-out ${isVisible ? "" : "fade-out"}`}>
+      <Container className="container-fluid">
+        <Row>
+          {/* Favorites List */}
+          <Col lg={4} xs={12} className="order-2 order-lg-1">
+            <div className="favorites-list" style={{marginLeft: '11%', overflowY: 'auto'}}>
+              <h1 style={{fontSize: '34px'}}>Favorites</h1>
+              {favorites.map((favorite) => (
+                <Row style={{cursor: 'pointer'}} key={favorite.id} onMouseEnter={() => {setToRender(favorite); setPostId(favorite.id)}} onClick={renderHandler}>
+                  <div className='list-text'>{favorite.reference}</div>
+                </Row>
+              ))}
             </div>
-            :
-            selected === null ?
-              <h1 className='favorites-page-headers'>Select a favorite</h1>
+          </Col>
+
+          {/* Card Display */}
+          <Col lg={8} xs={12} className="order-1 order-lg-2">
+            {favorites.length === 0 ? 
+              <div className='favorites-page-headers'>
+                <h1>No favorites saved</h1>
+                <h2 style={{textAlign: 'center'}}>Use the <Link to="/text-compare/">Textual Comparison Page</Link> to search for passages</h2>
+              </div>
               :
-              <Card className='passage-card'>
-                <Card.Header style={{ textAlign: 'center' }}>
-                  <strong>{selected.source}</strong>
-                </Card.Header>
-                <Card.Body>
-                  <Card.Title style={{textAlign: 'center' }}>
-                    {selected.reference}
-                  </Card.Title>
-                  <Card.Text>{selected.text}</Card.Text>
-                </Card.Body>
-                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                  <Button className='passagecard-button' variant="dark" style={{margin: '3px'}} onClick={() => deleteFavorite(postId)}>Remove</Button>
-                </div>
-              </Card>
-          }
-        </Col>
-      </Row>
+              selected === null ?
+                <h1 className='favorites-page-headers'>Select a favorite</h1>
+                :
+                <Card className='passage-card'>
+                  <Card.Header style={{ textAlign: 'center' }}>
+                    <strong>{selected.source}</strong>
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title style={{textAlign: 'center' }}>
+                      {selected.reference}
+                    </Card.Title>
+                    <Card.Text>{selected.text}</Card.Text>
+                  </Card.Body>
+                  <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Button className='passagecard-button' variant="dark" style={{margin: '3px'}} onClick={() => deleteFavorite(postId)}>Remove</Button>
+                  </div>
+                </Card>
+            }
+          </Col>
+        </Row>
+      </Container>
     </div>
-  </div>
-</>
-      )
+  </>
+  )
 }
 
 export default FavoritesPage
