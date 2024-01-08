bbf7bd893affe9c4dab62cad21f8f86f275ca16b
BDubOne <bryan.t.bartell@gmail.com>
2024-01-08 08:30:00 -0600

    checking

---
+++ backend/user_app/views.py
@@ -0,0 +1,191 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User, Favorites, JournalEntries
+from .serializers import UserSerializer, FavoritesSerializer, JournalEntriesSerializer
+from rest_framework import status
+import requests
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_204_NO_CONTENT,
+    HTTP_404_NOT_FOUND,
+)
+
+
+class SignupView(APIView):
+    def post(self, request):
+        data = request.data
+
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            user = User.objects.create_user(**data)
+            token = Token.objects.create(user=user)
+            print(user, token.key)
+            return JsonResponse(
+                {"user": user.username, "token": token.key},
+                status=status.HTTP_201_CREATED,
+            )
+
+        return JsonResponse(
+            {"message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class LoginView(APIView):
+    def post(self, request):
+        data = request.data
+        username = data.get("username")
+        password = data.get("password")
+        print(username, password)
+        user = authenticate(username=username, password=password)
+        print(user)
+
+        if user is None:
+            return JsonResponse(
+                {"message": "Invalid user credentials provided"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        token = Token.objects.get_or_create(user=user)
+        token_str = token[0].key
+
+        return JsonResponse({"token": token_str, "user": user.username})
+
+
+class InfoView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return JsonResponse({"user_id": request.user.id, "username": request.user.username})
+
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return JsonResponse(
+            {"message": "logout successful."}, status=HTTP_204_NO_CONTENT
+        )
+
+
+class FavoritesView(APIView):
+    def get(self, request):
+        favorites = Favorites.objects.filter(user=request.user.id)
+        favorites_serialized = FavoritesSerializer(favorites, many=True)
+        return Response(favorites_serialized.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = request.data.copy()
+        data["user"] = request.user.id
+        new_favorite = FavoritesSerializer(data=data)
+        if new_favorite.is_valid():
+            new_favorite.save()
+            return Response(new_favorite.data, status=HTTP_201_CREATED)
+        else:
+            return Response(new_favorite.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class AFavoriteView(APIView):
+    def get(self, request, id):
+        try:
+            favorite = Favorites.objects.get(user=request.user.id, id=id)
+            favorite_serialized = FavoritesSerializer(favorite)
+            return Response(favorite_serialized.data, status=status.HTTP_200_OK)
+        except Favorites.DoesNotExist:
+            return Response(
+                {"message": "Favorite not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+    def delete(self, request, id):
+        try:
+            del_favorite = Favorites.objects.get(user=request.user.id, id=id)
+            del_favorite.delete()
+            return Response(
+                {"message": "Favorite deleted successfully"},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        except Favorites.DoesNotExist:
+            return Response(
+                {"message": "Favorite not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+
+class JournalView(APIView):
+    def get(self, request):
+        journal_entries = JournalEntries.objects.filter(user=request.user.id)
+
+        journal = JournalEntriesSerializer(journal_entries, many=True)
+        return Response(journal.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = request.data.copy()
+        data["user"] = request.user.id
+
+        new_journal_entry = JournalEntriesSerializer(data=data)
+        new_journal_entry.is_valid()
+        print(new_journal_entry.errors)
+        if new_journal_entry.is_valid():
+            new_journal_entry.save()
+            return Response(new_journal_entry.data, status=HTTP_201_CREATED)
+        # else:
+        #     return Response(new_journal_entry.errors, status=HTTP_400_BAD_REQUEST)
+        # return Response(True)
+
+
+class JournalEntryView(APIView):
+    def get(self, request, entry_id):
+        try:
+            journal_entry = JournalEntries.objects.get(
+                user=request.user.id, id=entry_id
+            )
+            serialized_entry = JournalEntriesSerializer(journal_entry)
+            return Response(serialized_entry.data, status=status.HTTP_200_OK)
+        except JournalEntries.DoesNotExist:
+            return Response(
+                {"message": "Journal entry not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+    def put(self, request, entry_id):
+        try:
+            data = request.data.copy()
+            journal_entry = JournalEntries.objects.get(
+                user=request.user.id, id=entry_id
+            )
+            data["user"] = request.user.id
+            serializer = JournalEntriesSerializer(journal_entry, data=data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except JournalEntries.DoesNotExist:
+            return Response(
+                {"message": "Journal entry not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+    def delete(self, request, entry_id):
+        try:
+            journal_entry = JournalEntries.objects.get(
+                user=request.user.id, id=entry_id
+            )
+            journal_entry.delete()
+            return Response(
+                {"message": "Journal entry deleted successfully"},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        except JournalEntries.DoesNotExist:
+            return Response(
+                {"message": "Journal entry not found"}, status=status.HTTP_404_NOT_FOUND
+            )
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,76 @@
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import { useState} from 'react';
+import { Link, useOutletContext } from 'react-router-dom'
+import LogoWithoutText from '../Images/LogoWithoutText.png'
+import { api } from "../utilities/ApiUtilities"
+import { useNavigate } from 'react-router-dom';
+
+function LoginPage() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [wrongCredential, setWrongCredential] = useState(false)
+  const {setUser} = useOutletContext()
+  const navigate = useNavigate()
+
+  const login = async(e) => {
+    e.preventDefault()
+    let response = await api
+    .post("user/login/", {
+      username: username,
+      password: password
+    })
+    .catch((err)=>{
+      if (err.response.status===401){
+        localStorage.removeItem("token")
+        setWrongCredential(true)
+      }
+    })
+    if (response.status === 200) {
+      setUser(response.data.username);
+      localStorage.setItem("token", response.data.token)
+      api.defaults.headers.common[
+        "Authorization"
+      ] = `Token ${response.data.token}`;
+      navigate("/")
+    } else {
+      localStorage.clear()
+      alert('something went wrong')
+    }
+  }
+
+  return (
+      <>
+      <div className='form-page'>
+      <h1 className='form-page-headers'>Sign In</h1>
+      <Form onSubmit={(e)=>login(e)}>
+            <FloatingLabel
+            controlId="floatingInput"
+            label="Username" 
+            className="mb-3"
+            >
+              <Form.Control type="username" placeholder="name@example.com" onChange={(e)=>setUsername(e.target.value)}/>
+            </FloatingLabel>
+            <FloatingLabel controlId="floatingPassword" label="Password">
+                <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+            </FloatingLabel>
+            <Button className='form-buttons' variant='none' as="input" type="submit" value="Login"/>{' '}
+        </Form>
+        <div style={{color:'#FF000D', fontSize:'20px', alignSelf:'center', margin:'0px'}}>
+      {wrongCredential ? 
+      'Username or Password Incorrect.'
+      : 
+      null
+      }
+      </div>
+        <div style={{justifySelf:'center'}}>
+        {"Don't have an account?"} {<Link to={'/register/'}>Click here</Link>} to register.
+        </div>
+      </div>
+
+      </>
+  )
+}
+
+export default LoginPage
