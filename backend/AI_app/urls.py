fb639d85effeba511323e00b4c081e1c1db29c0a
BDubOne <bryan.t.bartell@gmail.com>
2024-01-08 08:29:33 -0600

    fixed

--- backend/AI_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import WordAnalysis, Commentary
-
-urlpatterns = [
-path("word/<str:word>/", WordAnalysis.as_view(), name="word analysis"),
-path("commentary/<str:Book>/<str:Chapter>/<str:Verse>/", Commentary.as_view(), name="commentary"),
-]
